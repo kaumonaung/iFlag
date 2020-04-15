17f4c5bf0c695158ec65c23dce9389fbb6d8b1e9
kaumonaung <ag.kaumon@gmail.com>
2020-04-15 18:27:23 +0200

    Initial Commit

---
+++ iFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iFlag
+//
+//  Created by Kaumon Aung on 15.04.20.
+//  Copyright © 2020 Kaumon Aung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
